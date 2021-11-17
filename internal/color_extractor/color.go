cee555501292757faaa0d7286232cf30c316e635
mishamyrt <misha@myrt.co>
2021-11-17 20:34:49 +0400

    Fix brightness limiter

--- internal/color_extractor/color.go
+++ internal/color_extractor/color.go
@@ -8,7 +8,7 @@ import (
 
 func CalcBrightness(c hass.RGBColor, minimum uint32) uint32 {
 	return uint32(
-		math.Min(
+		math.Max(
 			math.Max(float64(c[0]), math.Max(float64(c[1]), float64(c[2]))),
 			float64(minimum),
 		),
